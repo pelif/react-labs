41e986b321c78c73534d2c1ad933208630464d17
Felipe Daniel de Oliveira <felipe.dandev@gmail.com>
2024-09-10 14:16:49 -0300

    bkp estudos

--- miniblog/src/hooks/useFetchDocuments.js
+++ miniblog/src/hooks/useFetchDocuments.js
@@ -35,7 +35,17 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
 
                 //dashboard
 
-                q = await query(collectionRef, orderBy('createdAt', 'desc'));
+                if(search) {
+                    q = await query(
+                        collectionRef, 
+                        where("tagsArray", "array-contains", search), 
+                        orderBy("createdAt", "desc")
+                    );
+                } else {
+                    q = await query(collectionRef, orderBy('createdAt', 'desc'));
+                }
+
+                
 
                 await onSnapshot(q, (querySnapshot) => {
                     setDocuments(
---
+++ miniblog/src/pages/Search/Search.js
@@ -0,0 +1,36 @@
+import styles from './Search.module.css';
+import React from 'react'; 
+
+//hooks
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { useQuery } from '../../hooks/useQuery';
+
+//Components
+import PostDetail from '../../components/PostDetail';
+import { Link } from 'react-router-dom';
+
+const Search = () => {
+
+    const query = useQuery();
+    const search = query.get("q");
+
+    const { documents: posts } = useFetchDocuments("posts");
+
+    return (
+        <div className={styles.search_container}>
+            <h2>Search</h2>
+            <div>
+                {posts && posts.length === 0 && (
+                    <>
+                        <p>Não foram encontrados posts a partir da sua busca ...</p>
+                        <Link to="/" className="btn btn-dark">Voltar</Link>
+                    </>
+                )}
+                {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+            </div>
+        </div>
+    )
+}; 
+
+export default Search; 
+
